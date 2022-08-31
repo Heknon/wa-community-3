b20d4cbfe4af4217c0e088497d013c6f106a5038
Heknon <development@heknon.com>
2022-08-31 19:55:30 +0300

    fixed terribly buggy coinbomb

--- lib/economy/items/coinbomb.ts
+++ lib/economy/items/coinbomb.ts
@@ -6,6 +6,7 @@ import {pluralForm, waitForMessage} from "../../utils/message_utils";
 import {getUserRandom} from "../../user/user";
 import {weightedReward} from "../../command/commands/economy/utils";
 import {commas} from "../../utils/utils";
+import {prisma} from "../../db/client";
 
 export class CoinBomb extends Item {
     private static language = languages.items["coinbomb"];
@@ -31,17 +32,23 @@ export class CoinBomb extends Item {
                 },
             },
         );
-        waitForMessage((msg) => {
+        waitForMessage(async (msg) => {
             if (msg.jid !== message?.jid) {
                 return false;
             }
+
             if (coinsRewardAmount <= 0) {
                 message?.reply(CoinBomb.language.ended[chat.language], true, {
                     placeholder: {
                         custom: {
-                            total: commas(coinsRewardAmount - coinsLeft),
+                            collected: commas(coinsRewardAmount - coinsLeft),
+                            total: commas(coinsRewardAmount),
                             coins: pluralForm(
                                 coinsRewardAmount - coinsLeft,
+                                languages.economy.coin[chat.language],
+                            ),
+                            coins_total: pluralForm(
+                                coinsRewardAmount,
                                 languages.economy.coin[chat.language],
                             ),
                         },
@@ -49,38 +56,56 @@ export class CoinBomb extends Item {
                 });
                 return true;
             }
+            if (msg.fromBot) {
+                return false;
+            }
             if (msg.senderJid === executor.jid && msg.mentions.includes(executor.jid)) {
                 message?.reply(CoinBomb.language.greed[chat.language], true);
                 return false;
             }
 
-            if (msg.mentions.includes(executor.jid)) {
-                let coinsToGive = Math.floor(
-                    weightedReward(random, [
-                        [[0.05, 0.1], 0.2],
-                        [[0.01, 0.05], 0.1],
-                        [[0.1, 0.2], 0.4],
-                        [[0.15, 0.2], 0.2],
-                    ]) * coinsRewardAmount,
-                );
-                coinsToGive = coinsLeft >= 1000 ? Math.min(coinsToGive, coinsLeft) : coinsLeft;
-                msg.reply(CoinBomb.language.success[chat.language], true, {
-                    placeholder: {
-                        custom: {
-                            amount: commas(coinsToGive),
-                            coins: pluralForm(coinsToGive, languages.economy.coin[chat.language]),
-                        },
+            if (!msg.mentions.includes(executor.jid)) return false;
+
+            let coinsToGive = Math.floor(
+                weightedReward(random, [
+                    [[0.05, 0.1], 0.2],
+                    [[0.01, 0.05], 0.1],
+                    [[0.1, 0.2], 0.4],
+                    [[0.15, 0.2], 0.2],
+                ]) * coinsRewardAmount,
+            );
+            coinsToGive = coinsLeft >= 1000 ? Math.min(coinsToGive, coinsLeft) : coinsLeft;
+            coinsLeft -= coinsToGive;
+            await prisma.money.update({
+                where: {id: executor.money?.id!},
+                data: {
+                    wallet: {
+                        increment: coinsToGive,
                     },
-                });
-            }
+                },
+            });
+            msg.reply(CoinBomb.language.success[chat.language], true, {
+                placeholder: {
+                    custom: {
+                        amount: commas(coinsToGive),
+                        coins: pluralForm(coinsToGive, languages.economy.coin[chat.language]),
+                    },
+                },
+            });
+
             return false;
         }, 60 * 1000).catch((res) => {
             message?.reply(CoinBomb.language.ended[chat.language], true, {
                 placeholder: {
                     custom: {
-                        total: commas(coinsRewardAmount - coinsLeft),
+                        collected: commas(coinsRewardAmount - coinsLeft),
+                        total: commas(coinsRewardAmount),
                         coins: pluralForm(
                             coinsRewardAmount - coinsLeft,
+                            languages.economy.coin[chat.language],
+                        ),
+                        coins_total: pluralForm(
+                            coinsRewardAmount,
                             languages.economy.coin[chat.language],
                         ),
                     },
